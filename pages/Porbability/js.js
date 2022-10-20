191c63cb03cfc4364bbbece0727e315b8c7079dd
Prashan <76526590+prashanrai37@users.noreply.github.com>
2022-10-20 16:33:40 +0100

    Commit

---
+++ pages/Porbability/js.js
@@ -0,0 +1,28 @@
+function check() {
+  var c = 0;
+  var q1 = document.mm_quiz.question1.value;
+  var q2 = document.mm_quiz.question2.value;
+  var q3 = document.mm_quiz.question3.value;
+
+  if (q1 == '1a') {
+    c++;
+  }
+  if (q2 == '2c') {
+    c++;
+  }
+  if (q3 == '3a') {
+    c++;
+  }
+
+  if (c < 3) {
+    document.getElementById('result').style.backgroundColor = '#fff';
+    document.getElementById(
+      'result'
+    ).innerText = `You got ${c} out of 3. Try again.`;
+  } else {
+    document.getElementById('result').style.backgroundColor = '#fff';
+    document.getElementById(
+      'result'
+    ).innerText = `Congratulations! You got ${c} out of 3.`;
+  }
+}
